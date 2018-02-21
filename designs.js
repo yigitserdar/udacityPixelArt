a7977f2d971339aaf8ff479555bb6610ae6a7b1e
serdar <yigitserdar34@gmail.com>
2018-02-21 14:54:45 +0300

    add files

--- designs.js
+++ designs.js
@@ -1,30 +1,92 @@
-var input_rows;
-var input_cols;
-var input_color = "#000000";
+// drag and Draw functionality
 
-function makeGrid() {
-  var table = document.getElementById("pixel_canvas");
-  table.innerHTML = "";
-  while(table.rows.length > 0)
-    table.deleteRow(0);
-  input_rows = document.getElementById("input_height").value;
-  input_cols = document.getElementById("input_width").value;
-  for (var i = 0; i < input_rows; i++) {
-    var row_elem = table.insertRow(i);
-    row_elem.setAttribute("class", "row");
-    for (var j = 0; j < input_cols; j++) {
-      var cell= row_elem.insertCell(j);
-      cell.addEventListener('click', function(evt) {
-        evt.target.style.backgroundColor = document.getElementById("colorPicker").value;
-        this.style.borderColor="#9ecaed";
-        this.style.boxShadow="0 0 10px #9ecaed";
-      });
-    }
-  }
-  return false;
+function dragAndDraw() {
+    let down = false;
+    const td = $("td");
+    td.on("mousemove", function () {
+        if (down) {
+            let colorP = $("#colorPicker").val();
+            $(this).css("backgroundColor", colorP);
+        }
+    });
+
+    td.on("mousedown", function () {
+        down = true;
+    });
+
+    td.on("mouseup", function () {
+        down = false;
+    });
 }
 
-document.getElementById("sizePicker").addEventListener("submit", function(evt) {
-  evt.preventDefault();
-  makeGrid();
+//creating cells
+
+function cellsOnTable() {
+    // prompt user
+    let gridHeight = $("#inputHeight").val();
+    let gridWidth = $("#inputWidth").val();
+    //put cells on table
+    for (var i = 1; i <= gridHeight; i++) {
+        $("table").append("<tr></tr>");
+        for (var j = 1; j <= gridWidth; j++) {
+            $("tr:last").append("<td></td>");
+            $("td").attr("class", "canvas");
+        }
+    }
+}
+
+//creating cells randomly
+
+function randomCells() {
+    //get value randomly between 1 and 50
+    let gridHeightRandom = Math.floor(Math.random() * 51 + 1);
+    let gridWidthRandom = Math.floor(Math.random() * 51 + 1);
+    //put cells on table
+    for (var i = 1; i <= gridHeightRandom; i++) {
+        $("table").append("<tr></tr>");
+        for (var j = 1; j <= gridWidthRandom; j++) {
+            $("tr:last").append("<td></td>");
+            $("td").attr("class", "canvas");
+        }
+    }
+}
+
+//paint cells
+
+function paintCells() {
+    //paint cells
+    $(".canvas").on("click", function (event) {
+        let paint = $("#colorPicker").val();
+        $(event.target).css("background-color", paint);
+    });
+}
+
+// Pixel Art Maker via submit button
+
+$("#sizePicker").submit(function makeGrid(grid) {
+    //clear table
+    $("table tr").remove();
+    cellsOnTable();
+    dragAndDraw();
+    //make canvas appear
+    grid.preventDefault();
+    //make canvas background color white
+    $("td").css("background-color", "#fff");
+    //paint cells
+    paintCells();
+});
+
+// Pixel Art Maker via random button
+
+$("#submit-random").on("click", function makeGridRandom(grid) {
+    //clear table
+    $("table tr").remove();
+    randomCells();
+    dragAndDraw();
+    //make canvas appear
+    grid.preventDefault();
+    //make canvas background color white
+    $("td").css("background-color", "#fff");
+    //paint cells
+    paintCells();
 });
